b5493d62d56ae764bcf19ee95052715ac4c398ba
guidochavarris2 <lucanalopezguido@gmail.com>
2022-09-21 23:30:20 -0500

    first commit

---
+++ db_config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_USER', "CAMBIAR"); // Usuario
+define('DB_PASSWORD', "CAMBIAR"); // Password
+define('DB_DATABASE', "CAMBIAR"); // Nombre de la base de datos
+define('DB_SERVER', "CAMBIAR"); // db server
+?>
